78fe8e2ad9dac03ae432f22adab6e2f5ffcfc85b
Kevin C <KComaduran3@Outlook.com>
2026-04-02 18:23:14 -0500

    remove Auth code from file.

--- test2s.py
+++ test2s.py
@@ -20,7 +20,7 @@ URL = "https://data.usajobs.gov/api/search"
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
 # }
 
-headers = { 'Authorization-Key': '92z1IhbwjINTHyFmWLrNWdafaIEHdGdh9yiqP7K5UlI='}
+headers = { 'Authorization-Key': '='}
 # parms = {
 #      "Keyword:" "Data Analyst",
 #      #"WhoMayApply:" "Public",
